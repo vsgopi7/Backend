962e9f529af06fc668389d9382828301da85760a
Your Username <venkatasaigopi89@gmail.com>
2025-02-25 17:22:37 +0530

    commited

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-import express from 'express';
+
 import dotenv from 'dotenv';
 import connectDB from './db/index.js'; // Ensure the correct path and file extension
-
+import {app} from './app.js'; // Ensure the correct path and file extension
 dotenv.config();
 
-const app = express();
+
 const port = process.env.PORT || 3000;
 
-connectDB(); // Call the function to connect to the database
+connectDB()
+.then(()=>{
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+    });
+})
+.catch(error => console.error("MONGODB ERROR CONNECTION",error));  
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
 
